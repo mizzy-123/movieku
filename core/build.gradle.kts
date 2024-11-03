9ecbf4994e5683bdfa7a82b8d6368f559522becc
colondum <decarabian21@gmail.com>
2024-11-03 22:27:34 +0700

    add: proguard (masih ada problem)

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -28,7 +28,14 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -37,11 +44,11 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "1.8"
     }
 
     buildFeatures {
--- core/src/main/java/com/example/core/data/source/local/entity/GenreEntity.kt
+++ core/src/main/java/com/example/core/data/source/local/entity/GenreEntity.kt
@@ -3,7 +3,6 @@ package com.example.core.data.source.local.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import org.jetbrains.annotations.NotNull
 
 @Entity(tableName = "genre")
 data class GenreEntity(
